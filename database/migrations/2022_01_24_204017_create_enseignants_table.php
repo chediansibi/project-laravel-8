80150140249047f59146765b91f91a75ff2f81b7
chediansibi <nsibichedia93@gmail.com>
2022-01-25 22:14:10 +0100

    add file

---
+++ database/migrations/2022_01_24_204017_create_enseignants_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnseignantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enseignants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('adress');
+            $table->string('phone');
+            $table->string('subject');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enseignants');
+    }
+}
